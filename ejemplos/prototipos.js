cb19db56459ab69bdbe0f2e5e1467893ecc7fa68
Alex Just <alexjustdata@gmail.com>
2023-09-20 17:14:34 +0200

    add prototip

---
+++ ejemplos/prototipos.js
@@ -0,0 +1,74 @@
+// terminal --> npx nodemon prototipos.js
+
+
+'use strict';
+
+
+
+function Persona(nombre) {
+  this.nombre = nombre;
+  //this.saluda = function() { console.log('Hola soy', this.nombre); }
+}
+
+const pepe = new Persona('Pepe');  // objeto 1
+const luis = new Persona('Luis');  // objetos 2 ...
+
+/**
+ * Si yo quiero crear 10000 objetos, se crearán 10000 funciones para cada objeto
+ * y esto es ineficiente por memoria y tiempo
+ * 
+ * Usamos el metodo portotype
+ * de esta forma aunque hagamos millones de objetos todos tendrán el mismo prototipo
+ */
+
+Persona.prototype.saluda = function() { console.log('Hola soy', this.nombre); }
+
+/**
+ * JS se irá al objeto pepe (Persona) y verá que NO tiene una propiedad 'saluda'
+ * ñuego irá al prototipo y verá que SI tiene una propiedad 'saluda' pues usará el this. como pepe
+ */
+pepe.saluda();
+luis.saluda();
+
+
+
+
+// Herencia simple --------------------------
+console.log("Herencia simple ------------------")
+
+function Agente(nombre) {
+    // heredar el constructor de las Personas
+    // ejecutar el constructor de las Personas con mi "this"
+    Persona.call(this, nombre)
+  }
+  
+
+// HEREDAR las propiedades de la Clase Personas  a la Clase Agente --> heredeo metodo 'saluda'
+Object.setPrototypeOf(Agente.prototype, Persona.prototype);
+
+const smith = new Agente('Smith');
+
+smith.saluda();
+  
+
+// Herencia múltiple ------------------
+console.log("Herencia múltiple ------------------")
+
+// Quiero que los agentes hereden tanto de las personas, como de los superheroes
+// copiar todas las propiedades de los Superheroes al prototipo del Agente
+
+
+function Superheroe() {                                             // Definir una función constructora para Superheroe
+    this.vuela = function() { console.log(this.nombre, 'vuela'); }  // Método "vuela" que imprime el mensaje
+  }
+  
+ 
+Object.assign(Agente.prototype, new Superheroe());  // Asignar las propiedades de una nueva instancia de Superheroe al prototipo de Agente
+
+smith.vuela();  // llama al método "vuela" en el objeto "smith"
+
+
+console.log(smith);
+console.log(Agente.prototype);
+console.log(Persona.prototype);
+  
--- ejemplos/this.js
+++ ejemplos/this.js
@@ -1,18 +1,50 @@
 
-
 'use strict';
 
+/**
+ * Los métodos suelen utilizarse para definir operaciones que se pueden realizar en el objeto, 
+ * y típicamente tienen acceso a otras propiedades del objeto a través de la palabra clave this.
+ * 
+ * 
+ * La función Fruta actúa como un constructor para objetos de tipo Fruta. 
+ * Los objetos creados a partir de este constructor tendrán una propiedad nombre y un método saluda.
+ * 
+ * 
+ * 
+ */
 // crear una funciónm para usarla como constructor de objetos
-function Fruta(nombre) {
-  this.nombre = nombre;
-  this.saluda = function() {
+function Fruta1(nombre) {
+  this.nombre = nombre;                 // PROPIEDAD
+  this.saluda = function() {            // PROPIEDAD que es un MÉTODO (tiene una función)
     console.log('Hola soy', this.nombre);
   }
 }
 
-const limon = new Fruta('limon');
+/**
+ * Aquí tienes una versión ligeramente más moderna,  que utiliza la sintaxis de clases de ECMAScript 6
+ * 
+ * Define la clase Fruta
+ */
+class Fruta {                                // Constructor de la clase, se invoca al crear un nuevo objeto
+  constructor(nombre) {                      // Inicializa la propiedad "nombre" con el valor pasado como argumento
+    this.nombre = nombre;
+  }
 
-// limon.saluda();
+  saluda() {                                 // Define un método llamado "saluda"
+    console.log('Hola soy', this.nombre);    // Imprime un saludo que incluye la propiedad "nombre" del objeto
+  }
+}
+
+
+
+
+// creamos OBJETO limon con las PROPIEDADES nombre, saluda
+const limon = new Fruta1('limon');
+limon.saluda();
+
+const manzana = new Fruta2('Manzana');
+manzana.saluda();  // "Hola soy Manzana"
+
 
 // setTimeout(limon.saluda.bind(limon), 2000);
 
